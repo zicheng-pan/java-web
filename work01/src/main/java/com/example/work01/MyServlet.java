3feb7bad2a7ea0558c880296f6e0ca9d39fc04f5
zicheng <1094275633@qq.com>
2021-06-10 23:21:25 +0800

    initialize java-web project & commit first project

---
+++ work01/src/main/java/com/example/work01/MyServlet.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.work01;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Servlet 实现
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since TODO
+ */
+@WebServlet(urlPatterns = "/jumptojsp", asyncSupported = true)
+public class MyServlet extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // jump to jsp page
+        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+    }
+
+}
---
+++ work01/src/test/java/com/example/work01/Work01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
